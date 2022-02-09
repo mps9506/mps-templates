1195d13a0728bdf6694a9e7ae560b1bb65d33f86
mps9506 <mpschramm@gmail.com>
2022-02-09 16:04:33 -0600

    shorten file path refs

--- R/init_fonts.R
+++ R/init_fonts.R
@@ -1,9 +1,9 @@
 init_fonts <- function() {
   ## register fonts
-  NotoSansDisplay_Condensed_Regular <- fs::path_package("mpsTemplates", "fonts", "NotoSansDisplay_Condensed", "NotoSansDisplay_Condensed-Regular.ttf")
-  NotoSansDisplay_Condensed_Bold <- fs::path_package("mpsTemplates", "fonts", "NotoSansDisplay_Condensed", "NotoSansDisplay_Condensed-Bold.ttf")
-  NotoSansDisplay_Condensed_Italic <- fs::path_package("mpsTemplates", "fonts", "NotoSansDisplay_Condensed", "NotoSansDisplay_Condensed-Italic.ttf")
-  NotoSansDisplay_Condensed_BoldItalic <- fs::path_package("mpsTemplates", "fonts", "NotoSansDisplay_Condensed", "NotoSansDisplay_Condensed-BoldItalic.ttf")
+  NotoSansDisplay_Condensed_Regular <- fs::path_package("mpsTemplates", "fonts", "NSD_C", "NotoSansDisplay_Condensed-Regular.ttf")
+  NotoSansDisplay_Condensed_Bold <- fs::path_package("mpsTemplates", "fonts", "NSD_C", "NotoSansDisplay_Condensed-Bold.ttf")
+  NotoSansDisplay_Condensed_Italic <- fs::path_package("mpsTemplates", "fonts", "NSD_C", "NotoSansDisplay_Condensed-Italic.ttf")
+  NotoSansDisplay_Condensed_BoldItalic <- fs::path_package("mpsTemplates", "fonts", "NSD_C", "NotoSansDisplay_Condensed-BoldItalic.ttf")
 
   systemfonts::register_font(
     "NotoSansDisplay_Condensed",
@@ -13,10 +13,10 @@ init_fonts <- function() {
     bolditalic = NotoSansDisplay_Condensed_BoldItalic
   )
 
-  NotoSansDisplay_Regular <- fs::path_package("mpsTemplates", "fonts", "NotoSansDisplay", "NotoSansDisplay-Regular.ttf")
-  NotoSansDisplay_Bold <- fs::path_package("mpsTemplates", "fonts", "NotoSansDisplay", "NotoSansDisplay-Bold.ttf")
-  NotoSansDisplay_Italic <- fs::path_package("mpsTemplates", "fonts", "NotoSansDisplay", "NotoSansDisplay-Italic.ttf")
-  NotoSansDisplay_BoldItalic <- fs::path_package("mpsTemplates", "fonts", "NotoSansDisplay", "NotoSansDisplay-BoldItalic.ttf")
+  NotoSansDisplay_Regular <- fs::path_package("mpsTemplates", "fonts", "NSD", "NotoSansDisplay-Regular.ttf")
+  NotoSansDisplay_Bold <- fs::path_package("mpsTemplates", "fonts", "NSD", "NotoSansDisplay-Bold.ttf")
+  NotoSansDisplay_Italic <- fs::path_package("mpsTemplates", "fonts", "NSD", "NotoSansDisplay-Italic.ttf")
+  NotoSansDisplay_BoldItalic <- fs::path_package("mpsTemplates", "fonts", "NSD", "NotoSansDisplay-BoldItalic.ttf")
 
   systemfonts::register_font(
     "NotoSansDisplay",
